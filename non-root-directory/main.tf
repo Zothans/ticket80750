3ec303f7a77f7d0071a90462f381c96cbcb34f4e
Zothans <105400251+Zothans@users.noreply.github.com>
2022-08-23 14:15:49 +1000

    Update main.tf

--- non-root-directory/main.tf
+++ non-root-directory/main.tf
@@ -1,6 +1,16 @@
-resource "null_resource" "null" {
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.19.1"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
 }
 
 output "myoutput" {
-  value = "This run has been triggered from my non-root-directory"
+  value = "This run has been triggered from non-root-directory"
 }
