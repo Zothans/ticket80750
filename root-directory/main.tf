4c6a00e10b63af66ffdd9621c403b03b631b4844
Zothans <105400251+Zothans@users.noreply.github.com>
2022-08-23 14:16:21 +1000

    Update main.tf

--- root-directory/main.tf
+++ root-directory/main.tf
@@ -1,5 +1,12 @@
-resource "null_resource" "null" {
+terraform {
+  required_providers {
+    okta = {
+      source  = "okta/okta"
+      version = "~> 3.2"
+    }
+  }
 }
+
 
 output "myoutput" {
   value = "This run has been triggered from my root-directory"
