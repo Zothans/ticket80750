566441169454848ab1fe9b169cedfa61a1c73f26
Zothans <105400251+Zothans@users.noreply.github.com>
2022-08-23 14:26:04 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,14 +9,6 @@ terraform {
     }
   }
 }
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "3.19.1"
-    }
-  }
-}
 
 output "myoutput" {
   value = "This run has been triggered from root directory (/)"
