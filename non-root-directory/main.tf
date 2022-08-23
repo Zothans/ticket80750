bef08299eb03381caf227632c3333f92679dfb1b
Zothans <105400251+Zothans@users.noreply.github.com>
2022-08-23 14:25:04 +1000

    Update main.tf

--- non-root-directory/main.tf
+++ non-root-directory/main.tf
@@ -6,6 +6,14 @@ terraform {
     }
   }
 }
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.19.1"
+    }
+  }
+}
 
 provider "azurerm" {
   # Configuration options
